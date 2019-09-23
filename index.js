29787e995f3d49b1f8c93e45197099629ab90773
Dmytro Hnatiuk <dmytro.hnatiuk@synapse.com>
2019-09-23 18:58:37 +0300

    Fix them

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const fs = require('fs');
-const http1 = require('http');
+const express = require("express");
+const http = require('http');
 //Instanse
 const uuid = require('uuid/v4');
 const coding_message = require('./lib/code_data');
 
+const app = express();
+
+let code_message;
+
 require('./rand-shim.js');
 
-async function send_command_to_server(send_file) {
-    //const text_buff = fs.readFileSync(send_file);
+function send_command_to_server(send_file, callback) {
     const text_buff = send_file;
-
-    const req = http1.request({
+    const req = http.request({
         host: '80.91.165.208',
         path: '/er/cmd',
         headers: {
@@ -18,28 +20,44 @@ async function send_command_to_server(send_file) {
             'Content-Length': text_buff.length
         },
         method: 'POST'
-    },res => {
-        console.log(`Status Code: ${res.statusCode}`);
-        res.on('data', d => {
-            process.stdout.write(d);
+    },(res) => {
+        //console.log(`Status Code: ${res}`);
+        res.on('data', (data) => {
+            if(res.statusCode == 200){
+                //console.log(JSON.parse(data));
+                callback(data);
+            }
+            //callback(JSON.parse(d));
+            //process.stdout.write(d);
         });
         res.on('end', e =>{
-            console.log(`Status End: ${e}`);
+            //process.stdout.write(e);
+            //console.log(`Status End: ${e}`);
         })
     });
-
     req.on('error', error =>{
         console.error(`Error status ${error}`);
     });
-
     req.write(text_buff);
     req.end();
 }
 
 async function main() {
     await coding_message.load_key('Key-6.dat:tect4', '123.cer');
-    let code_message = await coding_message.package_command('{"Command":"Objects"}');
-    await send_command_to_server(code_message);
-    console.log(uuid());
+    code_message = await coding_message.package_command('{"Command":"Objects"}');
+    await send_command_to_server(code_message, (result) =>{
+        console.log(JSON.parse(result));
+        return result;
+    });
+    //console.log(uuid());
 }
-main();
+
+app.get("/", function(request, response){
+ main();
+    response.send("<h2>Привет Express!</h2>");
+    //response.send(data);
+});
+
+app.listen(3000);
+
+//main();
